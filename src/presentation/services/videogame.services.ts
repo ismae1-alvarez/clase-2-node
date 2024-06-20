d772519000b8fc6fd84a6d89da9d0172f02aa201
Ismael Alvarez <ismaelalvarez514@gmail.com>
2024-06-19 19:26:15 -0600

    dia 19 junio primero

--- src/presentation/services/videogame.services.ts
+++ src/presentation/services/videogame.services.ts
@@ -1,5 +1,10 @@
 import { Videogame } from "../../data"
 
+enum Status {
+    ACTIVE = 'ACTIVE',
+    INACTIVE = 'INACTIVE'
+  }
+
 
 export class VideogameServices{
     constructor (){}
@@ -25,7 +30,47 @@ export class VideogameServices{
 
     async finAllVideogame(){
         try {
-            return await  Videogame.find()
+            const videogame =  await  Videogame.find({
+                where :{
+                    status: Status.ACTIVE
+                }
+            })
+            if(!videogame){
+                throw new Error("El videojeugo no existe ")
+            }
+
+        } catch (error) {
+            throw new Error("Error")
+            console.log(error)
+        }
+
+        
+    }
+
+    async finAllVideogameById(id:number){
+        try {
+            return await  Videogame.findOne({
+                where : {
+                    id: id, 
+                    status : Status.ACTIVE
+                }
+            })
+        } catch (error:any) {
+            throw new Error("fdssfs")
+            console.log(error)
+        }
+    }
+
+    async updateVideogame(videogamData:any, id:number){
+        const videogame = await this.finAllVideogameById(id);
+
+        videogame!.title =  videogamData.name.toLowerCase().trim();
+        videogame!.description= videogamData.description.toLowerCase().trim();
+        videogame!.price = videogamData.price;
+
+        try {
+            await videogame?.save()
+            return videogame
         } catch (error) {
             console.log(error)
         }
--- src/presentation/videogames/controller.ts
+++ src/presentation/videogames/controller.ts
@@ -1,5 +1,6 @@
 import { Response, Request } from "express";
 import { VideogameServices } from "../services/videogame.services";
+import { json } from "stream/consumers";
 
 export class VideogamesController{
     constructor(
@@ -31,17 +32,44 @@ export class VideogamesController{
 
     getVideogamesById = (req:Request, res:Response) =>{
         const {id} =  req.params
-        return res.status(200).json({
-            messages : `videojuesgo con el ${id}`
-        })
+
+        if(isNaN(+id)){
+            return res.status(400).json({message : "El id no es un numero"})
+        }
+
+        this.videogameServices.finAllVideogameById(+id)
+            .then(videogame=>{  
+                return res.status(200).json(videogame)
+            })
+            .catch((err:any)=>{
+                console.log(err)
+                return res.status(500).json(err)
+            })
+        // return res.status(200).json({
+        //     messages : `videojuesgo con el ${id}`
+        // })
     }
 
     updateVideogamesById = (req:Request, res:Response) =>{
         const {id} =  req.params
         const {name, price, description} = req.body;
-        return res.status(200).json({
-            messages : `videojuesgo con el ${id} actulizo`
-        })
+
+        if(isNaN(+id)){
+            return res.status(400).json({message:"debe ser un numero"})
+        }
+
+        this.videogameServices.updateVideogame({name, price, description}, +id)
+            .then(videogame =>{
+                return res.status(200).json(videogame)
+            })
+            .catch((err:any)=>{
+                console.log(err)
+                return res.status(500).json(err)
+            })
+
+        // return res.status(200).json({
+        //     messages : `videojuesgo con el ${id} actulizo`
+        // })
     }
 
     deleteVideogamesById = (req:Request, res:Response) =>{
